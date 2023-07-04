ce6ca5e9f5147fe83e63871e576ad62d5281f38d
duarte-wallisson <wallisson.elduarte@gmail.com>
2023-07-04 11:27:05 -0300

    feat: implementing update product

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -34,6 +34,7 @@ import { MatSortModule } from '@angular/material/sort';
 
 import localePt from '@angular/common/locales/pt'
 import {registerLocaleData} from "@angular/common";
+import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
 
 registerLocaleData(localePt)
 @NgModule({
@@ -48,7 +49,8 @@ registerLocaleData(localePt)
         ForDirective,
         ProductCreateComponent,
         ProductReadComponent,
-        ProductRead2Component
+        ProductRead2Component,
+        ProductUpdateComponent
     ],
     imports: [
         BrowserModule,
